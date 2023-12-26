49ccf357c0d96d6802582fd7c24c2fb1aa5b4a58
macbook <gamerovichakbar@gmail.com>
2023-12-27 04:23:46 +0500

    almost done except focus and filtering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./styles/App/App.css";
+import Modal from "./components/Modal/Modal";
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [currentProduct, setCurrentProduct] = useState()
+  const [isModalActive, setIsModalActive] = useState(false)
+  const [ArrayOfTitles, setArrayOfTitles] = useState([]);
+  const [arr, setArr] = useState();
+  const [isLoaderStateActive, setIsLoaderStateActive] = useState(false);
+  const ProductsLink = "https://dummyjson.com/products";
+  const LoaderState = async () => {
+    setTimeout(() => {
+      setIsLoaderStateActive(!isLoaderStateActive);
+      fetchProductsDataFromQuery(ProductsLink);
+    }, 4000);
+  };
+  const fetchProductsDataFromQuery = async (link) => {
+    const data = await axios.get(link);
+    setArr(data.data.products);
+  };
+  useEffect(() => {
+    const inputElement = document.getElementsByClassName("input");
+    console.log(inputElement);
+    LoaderState();
+  }, []);
+  const openModalFunction = (product) => {
+    setIsModalActive(!isModalActive)
+    setCurrentProduct(product)
+  }
+  console.log(ArrayOfTitles);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="ProductCards">
+        {arr ? (
+          arr.map((product) => {
+            return (
+              <>
+                <div key={product.id} className="ProductCard">
+                  <h1 className="Title">{product.title}</h1>
+                  <p className="Description">{product.description}</p>
+                  <span className="Price">${product.price}</span>
+                  <img
+                    className="Image"
+                    src={product.thumbnail}
+                    alt="product-img"
+                  />
+                  <button
+                    onClick={() => openModalFunction(product) }
+                    className="Button"
+                  >
+                    Add
+                  </button>
+                </div>
+              </>
+            );
+          })
+        ) : (
+          <div>Загрузка...</div>
+        )}
+      </div>
+      <input
+        className="input"
+        onChange={(e) => setSearchValue(e.target.value)}
+        value={searchValue}
+      />
+      <div className="Cart">
+        Корзина:
+        {ArrayOfTitles.map((prodAdded) => {
+          return <p>{prodAdded}</p>;
+        })}
+      </div>
+      <Modal product={currentProduct} arr = {ArrayOfTitles} modalFunc = {setArrayOfTitles} state = {isModalActive} setState = {setIsModalActive} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Modal.css";
+const Modal = ({ product, state, setState, modalFunc, arr }) => {
+  console.log(state);
+  return (
+    <div className={state ? "Modal_layout_active" : "Modal_layout"}>
+      <div className="Modal_window">
+        <div className="Modal_info">
+          {product ? (
+            <div>
+              <p>{product.title}</p>
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+              <img src={product.thumbnail} alt="" />
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="Modal_buttons">
+          <button
+            onClick={() => {
+              modalFunc([...arr, product.title]);
+              return setState(!state);
+            }}
+          >
+            Add To cart
+          </button>
+          <button onClick={() => setState(!state)}>Return</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
